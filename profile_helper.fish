9eaa7520fb3573e0c146e408c6255dde3f6ee4dc
Damien DeVille <damien@ddeville.me>
2019-07-11 23:25:24 -0700

    Fix script

--- profile_helper.fish
+++ profile_helper.fish
@@ -1,8 +1,9 @@
 #!/usr/bin/env fish
 
 if test -e ~/.base16_theme
-  set -l SCRIPT_NAME (basename (realpath ~/.base16_theme) .sh)
-  source "../fish/$SCRIPT_NAME.fish"
+    set -l SCRIPT_DIR (realpath (dirname (status -f)))
+    set -l SCRIPT_NAME (basename (realpath ~/.base16_theme) .sh)
+    source "$SCRIPT_DIR/fish/$SCRIPT_NAME.fish"
 end
 
 
